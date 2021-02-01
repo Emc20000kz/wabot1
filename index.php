01e9582d180a4b988b6385341da080e13f04b510
Emc20000kz <dafaprayoga2005@gmail.com>
2021-02-01 13:45:59 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en" class="no-js">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="description" content="Bot Mini Game dan Moderasi Indonesia - Discord.">
+    <meta name=”robots” content="index, follow">
+
+    <meta property="og:title" content="Kazumi Bot WhatSapp">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="Bot WhatSapp dan Moderasi Indonesia">
+    <meta property="og:image" content="https://cdn.discordapp.com/attachments/566887245882064896/805689425027137576/unknown.png">
+    <meta property="og:url" content="https://botwhatsapp-kazumi.herokuapp.com/">
+    <meta name="twitter:card" content="summary_large_image">
+
+    <title>Kazumi WhatSapp Bot Indonesia</title>
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+
+    <link rel="stylesheet" href="css/style.css" />
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="sweetalert2/dist/sweetalert2.min.css">
+
+    <script type='text/javascript' src='./js/jpg.js'></script>
+    <script type='text/javascript' src='./js/pg.js'></script>
+    <script src="sweetalert2/dist/sweetalert2.min.js"></script>
+    <script type='text/javascript' src="js/option.js"></script>
+
+
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168942898-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-168942898-1');
+    </script>
+
+
+</head>
+
+<body>
+
+    <div id="particles">
+
+        <div id="intro">
+            <h1>Kazumi</h1>
+            <p>Bot WhatSapp Kazumi Dan Moderasi Indonesia</p>
+
+            <a href="https://wa.me/+6285707314250" class="btn" target="_blank">Nomer</a>
+            <a href="https://trakteer.id/daffap-qen7o/" class="btn" target="_blank">Donate</a>
+
+        </div>
+
+    </div>
+
+
+</body>
+
+</html>
